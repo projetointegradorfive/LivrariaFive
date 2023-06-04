25822f90574f26e5597d5e4a609857728d4ff046
Fernanda <projetointegradorfive@outlook.com>
2023-06-03 22:22:08 -0400

    ok

--- View/FormCarrinho.cs
+++ View/FormCarrinho.cs
@@ -37,21 +37,57 @@ namespace LivrariaFive.View
             // Inicializar o carrinho
             carrinho = new Carrinho();
 
-            // Carregar os itens do carrinho no DataGridView
-            //List<ItemDeCompra> itens = carrinhoController.ObterCarrinho(carrinho);
-
-            //if (itens.Count > 0)
-            //{
-            //    foreach (ItemDeCompra item in itens)
-            //    {
-            //        dataGridViewItensCarrinho.Rows.Add(item.Livro.Id, item.NomeLivro, item.PrecoLivro, item.Quantidade, item.Livro.Imagem);
-            //    }
-            //}
-            //else
-            //{
-            //    MessageBox.Show("O carrinho está vazio!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            //}
+            ConfigurarGrade();
+            // Carregar os itens do carrinho
+            CarregarItensCarrinho();
         }
+        private void ConfigurarGrade()
+        {
+            dgvCarrinho.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 12, FontStyle.Bold);
+            dgvCarrinho.DefaultCellStyle.Font = new Font("Arial", 12);
+            dgvCarrinho.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            dgvCarrinho.Columns["Imagem"].Width = 100;
+            dgvCarrinho.Columns["Imagem"].HeaderText = "Imagem";
+            dgvCarrinho.Columns["Imagem"].DefaultCellStyle.NullValue = null; // Define a célula vazia como nula
+            dgvCarrinho.Columns["Imagem"].ReadOnly = true; // Torna a coluna somente leitura
+
+            dgvCarrinho.Columns["Titulo"].Width = 300;
+            dgvCarrinho.Columns["Titulo"].HeaderText = "Título";
+            dgvCarrinho.Columns["Titulo"].ReadOnly = true; // Torna a coluna somente leitura
+
+            dgvCarrinho.Columns["Quantidade"].Width = 100;
+            dgvCarrinho.Columns["Quantidade"].HeaderText = "Quantidade";
+
+            dgvCarrinho.Columns["Preco"].Width = 100;
+            dgvCarrinho.Columns["Preco"].HeaderText = "Preço";
+            dgvCarrinho.Columns["Preco"].DefaultCellStyle.Format = "C2"; // Formato de moeda (R$)
+            dgvCarrinho.Columns["Preco"].ReadOnly = true; // Torna a coluna somente leitura
+        }
+
+
+        private void CarregarItensCarrinho()
+        {
+            dgvCarrinho.Rows.Clear();
+
+            foreach (ItemDeCompra item in carrinho.ItensDeCompra)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+
+                DataGridViewImageCell imageCell = new DataGridViewImageCell();
+                imageCell.ImageLayout = DataGridViewImageCellLayout.Zoom;
+                imageCell.Value = item.Livro.Imagem;
+                row.Cells.Add(imageCell);
+
+                row.Cells.Add(new DataGridViewTextBoxCell { Value = item.Livro.Titulo });
+                row.Cells.Add(new DataGridViewTextBoxCell { Value = item.Quantidade });
+                row.Cells.Add(new DataGridViewTextBoxCell { Value = item.PrecoLivro });
+
+                dgvCarrinho.Rows.Add(row);
+            }
+        }
+
+
 
     }
 }
